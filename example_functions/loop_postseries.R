7f15561ba9951bb04c25d131be74812cef34f71f
Gabriel Belle <g.belle@4intelligence.com.br>
2023-02-23 11:28:55 -0300

    ADD unidade de medida aberta

--- example_functions/loop_postseries.R
+++ example_functions/loop_postseries.R
@@ -28,53 +28,63 @@ token_prod = c(
 
 belle_path <- 'C:/Users/GabrielBelle/4intelligence/Feature Store - Documentos/DRE/Documentação/migracao'
 
-indicators_raw <- readxl::read_excel('grupos_transfs_FS.xlsx') %>%
-                  select(codigo, grupo, subgrupo, regioes)
-
-indicators <- indicators_raw %>%
-  filter(grupo == 'Juros') %>%
+indicators_raw <- readxl::read_excel(paste0(belle_path,
+                                            '/grupos_transfs_FS.xlsx')) %>% 
+  select(codigo, grupo, subgrupo, regioes, starts_with('original_'), starts_with('real_')) %>% 
   mutate(grupo = paste0(grupo, ' - ', subgrupo),
          regioes = ifelse(regioes == '0', '000', regioes))
 
-grupo_transf <- readxl::read_excel(paste0(belle_path,
+
+indicators <- indicators_raw %>% 
+  filter(codigo %in% c('BRFXR0015','BRFXR0021','BRFXR0005'))
+
+grupo_transf <- readxl::read_excel(paste0(belle_path, 
                                           '/diagrama_grupo_transfs.xlsx'),
-                                   sheet = 'depara') %>%
-  pivot_longer(cols = everything(),
-               names_to = 'grupo',
-               values_to = 'transfs') %>%
-  arrange(grupo) %>%
+                                   sheet = 'depara') %>% 
+  pivot_longer(cols = everything(), names_to = 'grupo', values_to = 'transfs') %>% 
+  arrange(grupo) %>% 
   filter(!is.na(transfs))
 
-indicators_to_send <- indicators %>%
-  left_join(grupo_transf) %>%
-  mutate(transf_sec = str_sub(transfs, 4,4),
-         unit_pt = case_when(
-           transf_sec == 'X' ~ '% a.d',
-           transf_sec == 'N' ~ '% a.a',
-           transf_sec == 'G' ~ '% a.a'
-         ),
-         unit_en = case_when(
-           transf_sec == 'X' ~ '% p.d',
-           transf_sec == 'N' ~ '% p.a',
-           transf_sec == 'G' ~ '% p.a'
-         ))
+depara_unidade <- readxl::read_excel(paste0(belle_path,
+                                            '/diagrama_grupo_transfs.xlsx'),
+                                     sheet = 'depara unidade medida') 
+
+indicators_to_send <- indicators %>% 
+  left_join(grupo_transf) %>% 
+  mutate(cod1_cod2 = paste0(str_sub(transfs, 1,1),
+                            transf_sec = str_sub(transfs, 4,4))) %>% 
+  left_join(depara_unidade) %>% 
+  rowwise() %>% 
+  mutate(un_pt = ifelse(is.na(un_pt),
+                        ifelse(str_sub(cod1_cod2,1,1) %in% c('O', 'S'), original_pt, real_pt),
+                        un_pt),
+         un_en = ifelse(is.na(un_en),
+                        ifelse(str_sub(cod1_cod2,1,1) %in% c('O', 'S'), original_en, real_en),
+                        un_en))
+
+if(any(is.na(indicators_to_send$un_pt)) | any(is.na(indicators_to_send$un_en))) {
+  na_un <- indicators_to_send %>% 
+    filter(is.na(un_pt) | is.na(un_en)) %>% 
+    distinct(codigo) %>% 
+    pluck('codigo')
+  
+  stop(paste0('Os seguintes indicadores não possuem unidade de medida preenchida: ', na_un))
+}
 
 for (i in unique(indicators_to_send$codigo)) {
-
   fsApi::delete_series(indicator = i,
                        token = token_homo)
-
-  df_filt <- indicators_to_send %>%
+  
+  df_filt <- indicators_to_send %>% 
     filter(codigo == i)
-
-  send_transf = fsApi::post.series(indicator_code = i,
+  
+  send_transf = post.series(indicator_code = i,
                             region_code = unique(df_filt$regioes),
                             transfs_code = unique(df_filt$transfs),
                             units_en = unique(df_filt$unit_en),
                             units_pt = unique(df_filt$unit_pt),
                             token = token_homo)
-
+  
 }
-
 
 
