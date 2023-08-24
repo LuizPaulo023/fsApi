320d3dbdc65c5a3e76b97539b5dd081cd5d40152
Luiz  Paulo T. Gonçalves <l.tavares@4intelligence.com.br>
2023-08-24 11:14:54 -0300

    CHANGED: bug loop

--- example_functions/loop_modify_indicator.R
+++ example_functions/loop_modify_indicator.R
@@ -2,6 +2,8 @@
 #' @author Luiz Paulo Tavares Gonçalves
 # Dependências/Pkgs
 
+rm(list = ls())
+
 library(tidyverse)
 library(stringr)
 library(stringi)
@@ -12,6 +14,7 @@ library(httr)
 user = base::getwd() %>%
        stringr::str_extract("^((?:[^/]*/){3})") %>% print()
 path <- '4intelligence/Feature Store - Documentos/DRE/Documentação/migracao/'
+
 
 # Definindo parâmetros do usuário na API - ambiente dev
 
@@ -25,7 +28,7 @@ url_dev = 'https://run-4i-dev-4casthub-featurestore-api-mhiml2nixa-ue.a.run.app/
 # Definindo parâmetros do usuário na API - ambiente Stg
 
 token_stg = c(
-  'Authorization' = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImpsUlBUc2FmM0MtZ3pITkdieTRYQSJ9.eyJodHRwczovLzRpbnRlbGxpZ2VuY2UuY29tLmJyL2VtYWlsIjoibC50YXZhcmVzQDRpbnRlbGxpZ2VuY2UuY29tLmJyIiwiaHR0cHM6Ly80aW50ZWxsaWdlbmNlLmNvbS5ici91c2VyX21ldGFkYXRhIjp7fSwiaHR0cHM6Ly80aW50ZWxsaWdlbmNlLmNvbS5ici9hcHBfbWV0YWRhdGEiOnsicm9sZXMiOlsiaXNGYWFTIiwiaXNGZWF0dXJlU3RvcmUiLCJpc0ZzQWRtaW4iXX0sImlzcyI6Imh0dHBzOi8vaG9tb2xvZ2F0aW9uLTRpbnRlbGxpZ2VuY2UudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYzODdhOGY0OTExYmM1NjgyOTQyNWNmYSIsImF1ZCI6WyI0Y2FzdGh1YiIsImh0dHBzOi8vaG9tb2xvZ2F0aW9uLTRpbnRlbGxpZ2VuY2UudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY4NzUyMjQ3MywiZXhwIjoxNjg3NjA4ODczLCJhenAiOiJLUW1ldnV3SVFvNVl3S0Zvb0dDVXJVZnNFVWk4eUszNCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJwZXJtaXNzaW9ucyI6WyJjcmVhdGU6cHJvamVjdHMiLCJlZGl0OmFjY2Vzcy1ncm91cHMiLCJlZGl0OmluZGljYXRvcnMiLCJlZGl0Om15LWdyb3VwcyIsImVkaXQ6b2JzZXJ2YXRpb25zIiwiZWRpdDpwcmVkZWZpbmVkLWdyb3VwcyIsImVkaXQ6cHJvamVjdGlvbnMiLCJlZGl0OnNlcmllcyIsInJlYWQ6YWNjZXNzLWdyb3VwcyIsInJlYWQ6ZG9tYWlucyIsInJlYWQ6aW5kaWNhdG9ycyIsInJlYWQ6bXktZ3JvdXBzIiwicmVhZDpvYnNlcnZhdGlvbnMiLCJyZWFkOnByZWRlZmluZWQtZ3JvdXBzIiwicmVhZDpwcm9qZWN0aW9ucyIsInJlYWQ6cHJvamVjdHMiLCJyZWFkOnNlcmllcyJdfQ.MJoZDoa9PpKXfq1TQKr09SFEbKeuV7AjTeeJ-ypOcRgMgh-LqsDRjIIhtv8KcHbv_ba2ognTrEnGYYtm02UhbTmdfU1wVr6EzMHLF4Hm0veZ1CMzi6S5cnOU_u8kOlEqRWiSM_YhF4_vfu0zc-sM8wVSZNpHt41x3J3RBL8uirG77b6Jh4Ac2GMEHbrrcIcPLAt5e8naILmbgIbl3czOTokJFU26nLKA0eBL_iPpRB1S_qvyQY4ZlkTPB3Ryb-1QzO8NX2H9pOjMxOhBvia-PyUzBNczG-1R3mWajOeO3EEtXpQ-ImTdaAMlthJRHCBpiJ8o7k66DfRUQS00IVnDaQ',
+  'Authorization' = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImpsUlBUc2FmM0MtZ3pITkdieTRYQSJ9.eyJodHRwczovLzRpbnRlbGxpZ2VuY2UuY29tLmJyL2VtYWlsIjoibC50YXZhcmVzQDRpbnRlbGxpZ2VuY2UuY29tLmJyIiwiaHR0cHM6Ly80aW50ZWxsaWdlbmNlLmNvbS5ici91c2VyX21ldGFkYXRhIjp7fSwiaHR0cHM6Ly80aW50ZWxsaWdlbmNlLmNvbS5ici9hcHBfbWV0YWRhdGEiOnsicm9sZXMiOlsiaXNGYWFTIiwiaXNGZWF0dXJlU3RvcmUiLCJpc0ZzQWRtaW4iXX0sImh0dHBzOi8vNGludGVsbGlnZW5jZS5jb20uYnIvbmFtZSI6ImwudGF2YXJlc0A0aW50ZWxsaWdlbmNlLmNvbS5iciIsImlzcyI6Imh0dHBzOi8vaG9tb2xvZ2F0aW9uLTRpbnRlbGxpZ2VuY2UudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYzODdhOGY0OTExYmM1NjgyOTQyNWNmYSIsImF1ZCI6WyI0Y2FzdGh1YiIsImh0dHBzOi8vaG9tb2xvZ2F0aW9uLTRpbnRlbGxpZ2VuY2UudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5Mjg3OTAzNCwiZXhwIjoxNjkyOTY1NDM0LCJhenAiOiJLUW1ldnV3SVFvNVl3S0Zvb0dDVXJVZnNFVWk4eUszNCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJwZXJtaXNzaW9ucyI6WyJjcmVhdGU6cHJvamVjdHMiLCJlZGl0OmFjY2Vzcy1ncm91cHMiLCJlZGl0OmluZGljYXRvcnMiLCJlZGl0Om15LWdyb3VwcyIsImVkaXQ6b2JzZXJ2YXRpb25zIiwiZWRpdDpwcmVkZWZpbmVkLWdyb3VwcyIsImVkaXQ6cHJvamVjdGlvbnMiLCJlZGl0OnNlcmllcyIsInJlYWQ6YWNjZXNzLWdyb3VwcyIsInJlYWQ6ZG9tYWlucyIsInJlYWQ6aW5kaWNhdG9ycyIsInJlYWQ6bXktZ3JvdXBzIiwicmVhZDpvYnNlcnZhdGlvbnMiLCJyZWFkOnByZWRlZmluZWQtZ3JvdXBzIiwicmVhZDpwcm9qZWN0aW9ucyIsInJlYWQ6cHJvamVjdHMiLCJyZWFkOnNlcmllcyJdfQ.TouyAaUI__rAGIC6n71WwffFqX4zsnB0sbBFIsKiMH8erkddTkXK7ynMGqrJ7UPSUUcCb7WH7pdkys6FUURe5jR_oJyt5xbg0OUG0nnbkC1vtv12NG0uRkekUAZP_ZT1JK8rPB8o1LT7_nHpw4MiCynGO4xT_CEuwqhDJQaEp1kZrWpaQA1j3w4aUB20wpHc0RB1Pcn8NeNDiPb9zZ07PEt1J9J7to9jW-H5PqTuM4IyJEW9M9fTeehqmplPJQOl-udt5fPtAUBt-WI9mWbQ6stEBVLjbwhEKKwrA54XQezNVVXt5z4DtgzYtzZOcKRBLj4OCv4keBEhW1wqBUFNrw',
   'Content-Type' = 'application/json'
 )
 
@@ -42,7 +45,7 @@ url_prod = 'https://run-prod-4casthub-featurestore-api-zdfk3g7cpq-ue.a.run.app/'
 
 # Definindo o ambiente ---------------------------------------------------------
 
-ambiente = 'prod' #\ opções: dev, stg, prod
+ambiente = 'stg' #\ opções: dev, stg, prod
 
 # Configurando a url e token de acordo com o ambiente escolhido
 
@@ -72,6 +75,12 @@ metadados <- readxl::read_excel(paste0(user,
                                 skip = 1) %>%
              janitor::clean_names()
 
+sends_poc <- readxl::read_excel("poc_chatGPT_lote.xlsx", sheet = "verificado") %>% 
+              dplyr::filter(sends == "ok") %>% 
+               janitor::clean_names()
+
+
+metadados <- dplyr::left_join(sends_poc, metadados, by = "indicator_code")
 
 # Limpando e organizando metadados ---------------------------------------------
 # Exemplo retirado do script: loop_postindicators
@@ -92,9 +101,10 @@ metadados_filt <- metadados %>%
     across(where(is.character), ~gsub('\n','', .x)),
     across(starts_with('description'), ~str_replace_all(.x, '"',"'"))
   ) %>%
-  filter(in_fs == F) %>% #\ Presentes na FS
-  filter(sera_migrado == 'TRUE') %>% 
-  filter(crawler == 'tesouro')
+  filter(in_fs == F)  %>% slice(1:10)
+#\ Presentes na FS
+  # filter(sera_migrado == 'TRUE') %>% 
+  # filter(crawler == 'tesouro')
 
 # Loop indicadores -------------------------------------------------------------
   
@@ -104,45 +114,37 @@ for (r in 1:nrow(metadados_filt)) {
 
   print(metadados_filt[r,'indicator_code'])
   
-  if(metadados_filt[r, 'in_fs']$in_fs) {
-    proj = '4intelligence'
-  } else {
-    proj = ' '
-  }
+  # if(metadados_filt[r, 'in_fs']$in_fs) {
+  #   proj = '4intelligence'
+  # } else {
+  #   proj = ' '
+  # }
 
   # Chamando a função modificação
-  status <- modify_indicator(
-    access_type = "default",
-    access_group = "Geral",
-    indicator = metadados_filt[r, 'indicator_code'][[1]],
-    name_en = metadados_filt[r, 'name_en_fs'][[1]],
-    name_pt = metadados_filt[r, 'name_pt_fs'][[1]],
-    short_en = metadados_filt[r, 'name_abv_en_fs'][[1]],
-    short_pt = metadados_filt[r, 'name_abv_pt_fs'][[1]],
-    source_en = metadados_filt[r, 'fonte_fs_en'][[1]],
-    source_pt = metadados_filt[r, 'fonte_fs_pt'][[1]],
-    description_en =  metadados_filt[r, 'description_en_fs'][[1]],
-    description_pt =  metadados_filt[r, 'description_pt_fs'][[1]],
-    description_full_en = metadados_filt[r, 'link_metodologia_fs_en'][[1]],
-    description_full_pt = metadados_filt[r, 'link_metodologia_fs_pt'][[1]],
-    country = str_sub(metadados_filt[r, 'indicator_code'],1,2)[[1]],
-    sector = str_sub(metadados_filt[r, 'indicator_code'],3,5)[[1]],
-    node_en = str_split(metadados_filt[r, 'tree_en_fs'][[1]],
-                        ",")[[1]],
-    node_pt = str_split(metadados_filt[r, 'tree_pt_fs'][[1]],
-                        ",")[[1]],
-    proj_owner = proj,
-    token = token_to_use,
-    url = url_to_use)
-  
-  # if(any(status != 200)) {
-  #   problem_sids <- problem_sids %>% 
-  #     add_row(sids = metadados_filt[r, 'indicator_code'][[1]])
-  # }
-  
+  status <- post.indicator(access_type = "default",
+                           access_group = "Geral",
+                           indicator_code = metadados_filt[r, 'indicator_code'][[1]],
+                           name_en = metadados_filt[r, 'name_en_fs'][[1]],
+                           name_pt = metadados_filt[r, 'name_pt_fs'][[1]],
+                           short_en = metadados_filt[r, 'name_abv_en_fs'][[1]],
+                           short_pt = metadados_filt[r, 'name_abv_pt_fs'][[1]],
+                           source_en = metadados_filt[r, 'fonte_fs_en'][[1]],
+                           source_pt = metadados_filt[r, 'fonte_fs_pt'][[1]],
+                           description_en = metadados_filt[r, 'en_us'][[1]],
+                           description_pt = metadados_filt[r, 'pt_br'][[1]],
+                           description_full_en = metadados_filt[r, 'link_metodologia_fs_en'][[1]],
+                           description_full_pt = metadados_filt[r, 'link_metodologia_fs_pt'][[1]],
+                           country = str_sub(metadados_filt[r, 'indicator_code'],1,2)[[1]],
+                           sector = str_sub(metadados_filt[r, 'indicator_code'],3,5)[[1]],
+                           node_en = str_split(metadados_filt[r, 'tree_en_fs'][[1]],
+                                               ",")[[1]],
+                           node_pt = str_split(metadados_filt[r, 'tree_pt_fs'][[1]],
+                                               ",")[[1]],
+                           type_send = 'PUT', #ou PUT
+                           token = token_to_use,
+                           proj_owner = proj,
+                           url = url_to_use)
 }
-
-
 ########################################################
 
 access_type = "default"
@@ -154,8 +156,10 @@ short_en = metadados_filt[r, 'name_abv_en_fs'][[1]]
 short_pt = metadados_filt[r, 'name_abv_pt_fs'][[1]]
 source_en = metadados_filt[r, 'fonte_fs_en'][[1]]
 source_pt = metadados_filt[r, 'fonte_fs_pt'][[1]]
-description_en =  metadados_filt[r, 'description_en_fs'][[1]]
-description_pt =  metadados_filt[r, 'description_pt_fs'][[1]]
+description_en = metadados_filt[r, 'en_us'][[1]]
+description_pt = metadados_filt[r, 'pt_br'][[1]]
+# description_en =  metadados_filt[r, 'description_en_fs'][[1]]
+# description_pt =  metadados_filt[r, 'description_pt_fs'][[1]]
 description_full_en = metadados_filt[r, 'link_metodologia_fs_en'][[1]]
 description_full_pt = metadados_filt[r, 'link_metodologia_fs_pt'][[1]]
 country = str_sub(metadados_filt[r, 'indicator_code'],1,2)[[1]]
