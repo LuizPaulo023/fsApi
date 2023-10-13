10f31795197b31ed9e74a687a1fd71f94228aa2c
gabBelle <g.belle@4intelligence.com.br>
2023-10-13 19:10:38 -0300

    ADD: stop if error

--- R/delete_series.R
+++ R/delete_series.R
@@ -16,9 +16,9 @@
 
 select_sids_to_del <- function(indicator,
                                new_sids,
-                               token, 
+                               token,
                                url) {
-  
+
   current_series_raw <- httr::VERB("GET",
                                    url = paste0(url, "api/v1/indicators/", indicator, "/series?limit=2000"),
                                    add_headers(token)) %>% content("parsed")
@@ -26,19 +26,19 @@ select_sids_to_del <- function(indicator,
     return(character(0))
   }
   current_series <- tibble()
-  
+
   for (i in 1:length(current_series_raw[['data']])) {
     sid = current_series_raw[['data']][[i]][['code']]
-    
+
     current_series <- current_series %>%
       bind_rows(tibble(sid = sid))
   }
-  
+
   current_series <- current_series %>%
     pluck('sid')
-  
+
   del_series <- current_series[!(current_series %in% new_sids$sid)]
-  
+
   return(del_series)
 }
 
@@ -46,7 +46,9 @@ delete_series <- function(indicator,
                           del_series,
                           token,
                           url){
-  
+
+  result = tibble(out = c())
+
   for (sid in del_series) {
     delete_series <- httr::VERB(
       "DELETE",
@@ -56,8 +58,16 @@ delete_series <- function(indicator,
                    sid),
       add_headers(token)
     )
+
+    result <- result %>%
+      bind_rows(tibble(out = delete_series$status_code))
+
     print(sid)
     cat(httr::content(delete_series, 'text'))
-    Sys.sleep(0.25)
+    Sys.sleep(0.05)
+  }
+
+  if(any(!(result$out %in% c(200, 404)))) {
+    stop('problema no envio')
   }
 }
