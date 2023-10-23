798cdfab5e2fcf8d45495f9070b68923058e177d
gabBelle <g.belle@4intelligence.com.br>
2023-10-23 16:38:38 -0300

    ADD: parametro access_type

--- R/modify_series.R
+++ R/modify_series.R
@@ -1,35 +1,37 @@
-#' @title Função Update Series 
-#' @author Luiz Paulo T. 
+#' @title Função Update Series
+#' @author Luiz Paulo T.
 
-modify_series <- function(sid, units_en, units_pt, token, url){
-  
-    # Guardando os inputs 
+modify_series <- function(sid, tipo_acesso = 'default' units_en, units_pt, token, url){
+
+    # Guardando os inputs
     input <- tibble::tibble(sid, units_en, units_pt)
-  
-# Campos da API para preenchiemnto  
-  
+
+# Campos da API para preenchiemnto
+
 body = '{
   "status": "active",
+  "access_type": "step_one",
   "unit": {
-    "en-us": "step_one",
-    "pt-br": "step_two"
+    "en-us": "step_two",
+    "pt-br": "step_three"
   }
 }';
 
-# Preenchendo o JSON - API 
+# Preenchendo o JSON - API
 
 send_fs = input %>%
           dplyr::mutate(indicador = substr(sid, 1,9)) %>%
           dplyr::rowwise() %>%
           dplyr::mutate(body = body,
-                        body_json = stringr::str_replace_all(body, c("step_one" = units_en,
-                                                                     "step_two" = units_pt)),
+                        body_json = stringr::str_replace_all(body, c("step_two" = units_en,
+                                                                     "step_three" = units_pt,
+                                                                     "step_one" = tipo_acesso)),
                         url = paste0(url, 'api/v1/indicators/', indicador, "/series/", sid)) %>% dplyr::select(-body)
 
-# \Loop: modify_series 
+# \Loop: modify_series
 
 for (i in 1:length(send_fs$body_json)) {
-  
+
   path_series <- VERB("PATCH",
                       url = send_fs$url[i],
                       body = send_fs$body_json[i],
@@ -40,31 +42,31 @@ for (i in 1:length(send_fs$body_json)) {
     cat(httr::content(path_series, "text"))
     cat("\n")
     cat("\n")
-    
+
     return(path_series$status_code)
   }
 
 }
 
 get_sids <- function(url, token, indicators){
-  
+
   all_series <- tibble()# para preenchimento
-  
+
   for (indicator in indicators) {
     get_fs <- httr::VERB("GET",
                          url = paste0(url, "api/v1/indicators/", indicator, "/series?limit=3000"),
                          add_headers(token)) %>%
       httr::content("parsed")
-    
+
     current_series <- tibble()
     for (i in seq_along(get_fs[["data"]])) {
       current_code <- get_fs[["data"]][[i]][["code"]]
       current_series <- dplyr::bind_rows(current_series,
                                          tibble(code = current_code))
     }
-    
+
     all_series <- dplyr::bind_rows(all_series, current_series)
   }
-  
+
   return(all_series)  # Retorna o data frame com todas as séries
 }
